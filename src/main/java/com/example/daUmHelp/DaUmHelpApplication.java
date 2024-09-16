f1ef7d451c61a683c662b2319dd6559f1225b619
davi.paula <davi.paula@defensoria.mg.def.br>
2024-09-16 11:38:49 -0300

    initial commit

---
+++ src/main/java/com/example/daUmHelp/DaUmHelpApplication.java
@@ -0,0 +1,13 @@
+package com.example.daUmHelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaUmHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaUmHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daUmHelp/DaUmHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daUmHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaUmHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
