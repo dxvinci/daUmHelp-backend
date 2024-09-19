6cb0cfed957f4b0b79dfc6224441dc2addba71c1
Davi Ribeiro <davirpa1@gmail.com>
2024-09-19 16:16:39 -0300

    config: default endpoint routes

---
+++ src/main/java/com/example/daUmHelp/infra/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.daUmHelp.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api/v1", handlerType -> true);
+    }
+}
